e961f8d44c2e2189e282bfefe781b607b5c282d9
Didier <bider88@gmail.com>
2018-05-28 23:40:51 -0500

    Integrando input search y controles de busquedad

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.navbar.has-shadow
+        .container
+          .field-body
+            .field.has-addons
+              .control.is-expanded
+                input.input.is-large(type="text" placeholder="Buscar canciones" v-model="searchQuery")
+              .control
+                a.button.is-info.is-large(@click="search") Buscar
+              .control
+                a.button.is-danger.is-large &times;
+          p(v-show="showResultMessage")
+            small {{ searchMessage }}
+      .container
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+
+const tracks = [
+  {
+    name: 'Cancion',
+    artist: 'Alguien'
+  },
+  {
+    name: 'Otra canción',
+    artist: 'Artista'
+  },
+  {
+    name: 'Deja de llorar',
+    artist: 'El dolido'
+  }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola vue!'
+      searchQuery: '',
+      tracks: [],
+      showResultMessage: false
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Se encontraron ${this.tracks.length} resultado${this.tracks.length !== 1 ? 's' : ''}`
+    }
+  },
+  methods: {
+    search () {
+      this.showResultMessage = true
+      this.tracks = tracks
     }
   }
 }
